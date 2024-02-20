cae4913c6ecc0df691256bd1f3825a513c1dd94b
Sarah Fillion <2330055@collegebart.ca>
2024-02-20 15:29:44 -0500

    Avancement dessin

--- src/lab3.1_entrypoint.c
+++ src/lab3.1_entrypoint.c
@@ -11,15 +11,19 @@ void raylib_start(void){
             break;
         }
     
-    
+        Color bleuDoux = (Color){0,100,200,255};
         BeginDrawing();
-        ClearBackground(BLACK);
+        ClearBackground(WHITE);
         
-        DrawRectangleLines(200,400,100,100,(Color){255,0,0,255});
-        DrawRectangleLines(212,325,75,75,(Color){255,0,0,255});
-        DrawRectangleLines(225,275,50,50,(Color){255,0,0,255});
+        DrawRectangleLines(200,400,100,100,bleuDoux);
+        DrawRectangleLines(212,325,75,75,bleuDoux);
+        DrawRectangleLines(225,275,50,50,bleuDoux);
 
-        DrawLine(210,275,290,275,(Color){255,0,0,255});
+        DrawLine(210,275,290,275,bleuDoux);
+        DrawRectangleLines(230,235,40,40,bleuDoux);
+
+        DrawLine(212,350,170,290,bleuDoux);
+        DrawLine(286,350,350,390,bleuDoux);
 
         EndDrawing();
     }
